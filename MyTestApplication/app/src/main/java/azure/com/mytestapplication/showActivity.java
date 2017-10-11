40503a65ab4bf1dffd05a3b2c2eeeba8ea305068
Cryrain <lvbinrain@gmail.com>
2017-10-11 23:19:10 +0800

    rr
    tt1rr

---
+++ MyTestApplication/app/src/main/java/azure/com/mytestapplication/showActivity.java
@@ -0,0 +1,49 @@
+package azure.com.mytestapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class showActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Log.v("d","dfgff");
+
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra(TestMainActivity.EXTRA_MESSAGE);
+
+        Log.v("d",text);
+
+
+        TextView showView = (TextView) findViewById(R.id.showView);
+        showView.setText(text);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
