34c5b4580d56350c09c785b4f2762cdc4bc2ab81
Cryrain <lvbinrain@gmail.com>
2017-10-13 18:05:05 +0800

    gps
    gps 1.0

--- MyTestApplication/app/src/main/java/azure/com/mytestapplication/showActivity.java
+++ MyTestApplication/app/src/main/java/azure/com/mytestapplication/showActivity.java
@@ -1,16 +1,28 @@
 package azure.com.mytestapplication;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class showActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +31,21 @@ public class showActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Log.v("d","dfgff");
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
 
         Intent intent = getIntent();
 
         String text = intent.getStringExtra(TestMainActivity.EXTRA_MESSAGE);
 
-        Log.v("d",text);
 
 
-        TextView showView = (TextView) findViewById(R.id.showView);
+        TextView showView = (TextView)findViewById(R.id.showView);
+        showView.setTextSize(TypedValue.COMPLEX_UNIT_PX,getResources().getDimension(R.dimen.text_size));
         showView.setText(text);
     }
 
@@ -45,5 +53,88 @@ public class showActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocationListener locationListener = new LocationTracker();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+    }
+
+    private class LocationTracker implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+            double longitude = location.getLongitude();
+            double laititude = location.getLatitude();
+
+            Log.v("Longitude",""+longitude);
+            Log.v("laititude",""+laititude);
+
+            Toast.makeText(getBaseContext(), "经度:"+longitude + " /n纬度:" + laititude,Toast.LENGTH_SHORT);
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+
+
+    public void showGps(View view){
+        Log.v("showGps:","show gps start");
+
+
+
+
+        boolean b2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+        Log.v("not use2",b2+"");
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        int i = 1;
+
+        if(location == null){
+            location =locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            i = 2;
+        }
+
+
+
+            double longitude = location.getLongitude();
+            double laititude = location.getLatitude();
+
+            Log.v("longitude:", ""+longitude);
+            Log.v("laititude:",""+laititude);
+
+
+            Toast toast = Toast.makeText(getBaseContext(),"longitude"+i+": "+longitude+"/n"+" laititude"+i+":"+laititude,Toast.LENGTH_SHORT);
+            toast.show();
+
+
+
+
+    }
+
+
 }
+
+
+
